3e3426537f09b3e62d5dfaa446e1134b7aa6b341
Nikodem Kastelik <nikodemkastelik@gmail.com>
2019-08-02 23:57:07 +0200

    3#: Expand example application to use nested states

--- src/main.c
+++ src/main.c
@@ -11,10 +11,12 @@ enum events
 };
 
 static bool s1_handler(hsm_event_t evt);
+static bool s11_handler(hsm_event_t evt);
 static bool s2_handler(hsm_event_t evt);
 
-static const hsm_state_t s1 = {.handler = s1_handler, .parent = NULL};
-static const hsm_state_t s2 = {.handler = s2_handler, .parent = NULL};
+static const hsm_state_t s1  = {.handler = s1_handler,  .parent = NULL};
+static const hsm_state_t s11 = {.handler = s11_handler, .parent = &s1};
+static const hsm_state_t s2  = {.handler = s2_handler,  .parent = NULL};
 
 bool s1_handler(hsm_event_t evt)
 {
@@ -24,10 +26,29 @@ bool s1_handler(hsm_event_t evt)
             printf("S1 entry\n");
             break;
         case EVENT_SWITCH_STATES:
-            hsm_transition(&hsm, &s2);
+            hsm_transition(&hsm, &s11);
             break;
         case HSM_EVENT_EXIT:
             printf("S1 exit\n");
+            break;
+        default:
+            return false;
+    }
+    return true;
+}
+
+bool s11_handler(hsm_event_t evt)
+{
+    switch (evt)
+    {
+        case HSM_EVENT_ENTRY:
+            printf("S11 entry\n");
+            break;
+        case EVENT_SWITCH_STATES:
+            hsm_transition(&hsm, &s2);
+            break;
+        case HSM_EVENT_EXIT:
+            printf("S11 exit\n");
             break;
         default:
             return false;
@@ -57,6 +78,7 @@ bool s2_handler(hsm_event_t evt)
 int main(void)
 {
     hsm_start(&hsm, &s1);
+    hsm_dispatch(&hsm, EVENT_SWITCH_STATES);
     hsm_dispatch(&hsm, EVENT_SWITCH_STATES);
     hsm_dispatch(&hsm, EVENT_SWITCH_STATES);
     hsm_dispatch(&hsm, EVENT_SWITCH_STATES);
